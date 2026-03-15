7487d0be3c8ab6799699f0e9bac9b533c618dc37
shoegaze <7885170+shoegaze@users.noreply.github.com>
2026-03-15 17:30:00 +0900

    add checks to *nix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,33 @@
+echo 'Copying vim config files to home...'
+
 # TODO: Use paths from env vars
-cp .vimrc ~/.vimrc
-cp .ideavimrc ~/.ideavimrc
-cp .config/nvim/init.vim ~/.config/nvim/init.vim
+export INSTALL_ROOT_DIR="~"
+export INSTALL_VIMRC_PATH="$INSTALL_ROOT_DIR"/.vimrc
+export INSTALL_IDEAVIMRC_PATH="$INSTALL_ROOT_DIR"/.ideavimrc
+export INSTALL_NVIM_PATH="$INSTALL_ROOT_DIR"/.config/nvim/init.vim
+
+
+# .vimrc
+echo "Copying .vimrc to path \"$INSTALL_VIMRC_PATH\""
+if cp .vimrc "$INSTALL_VIMRC_PATH" ; then
+  echo '=> Install successful'
+else
+  echo '=> Install failed!'
+fi
+
+# .ideavimrc
+echo "Copying .ideavimrc to path \"$INSTALL_IDEAVIMRC_PATH\""
+if cp .ideavimrc "$INSTALL_IDEAVIMRC_PATH" ; then
+  echo '=> Install successful'
+else
+  echo '=> Install failed!'
+fi
+
+# nvim/init.vim
+echo "Copying .config/nvim/init.vim to path \"$INSTALL_NVIM_PATH\""
+if cp .config/nvim/init.vim "$INSTALL_NVIM_PATH" ; then
+  echo '=> Install successful'
+else
+  echo '=> Install failed!'
+fi
 
